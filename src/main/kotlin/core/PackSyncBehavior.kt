f9aabba4a6053d65867067dd4054c1c12a8a1fce
Yaossg <yaossg@outlook.com>
2024-10-18 10:20:38 +0800

    add uncompressed for pack

--- src/main/kotlin/core/PackSyncBehavior.kt
+++ src/main/kotlin/core/PackSyncBehavior.kt
@@ -1,6 +1,7 @@
 package pikapack.core
 
 import pikapack.plan.SyncPlan
+import java.io.InputStream
 
 import java.security.MessageDigest
 import java.security.SecureRandom
@@ -8,19 +9,21 @@ import javax.crypto.*
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 
-import java.nio.file.Path
 import java.nio.file.Files
+import java.util.zip.CRC32
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 import java.util.zip.ZipInputStream
 import javax.crypto.Cipher
 import javax.crypto.CipherOutputStream
-import javax.xml.stream.events.Characters
 import kotlin.io.path.createDirectories
 import kotlin.io.path.createParentDirectories
 import kotlin.io.path.exists
+import kotlin.io.path.fileSize
+import kotlin.io.path.getLastModifiedTime
 import kotlin.io.path.outputStream
 import kotlin.io.path.inputStream
+import kotlin.io.path.setLastModifiedTime
 
 object PackSyncBehavior: SyncBehavior {
     private const val ALGORITHM = "AES"
@@ -28,12 +31,25 @@ object PackSyncBehavior: SyncBehavior {
     private const val TRANSFORMATION = "AES/CBC/PKCS5Padding"
     private const val IV_SIZE = 16
 
-    private fun inZipInput(plan: SyncPlan, zipIn : ZipInputStream, srcDir: Path) {
+    private fun readAllBytes(inputStream: InputStream, action: (buf: ByteArray, off: Int, len: Int) -> Unit) {
+        val buffer = ByteArray(4096)
+        var len: Int
+        while (inputStream.read(buffer).also { len = it } > 0) {
+            action(buffer, 0, len)
+        }
+    }
+
+    private fun inZipInput(plan: SyncPlan, zipIn : ZipInputStream) {
+        val srcDir = plan.options.src
         var entry : ZipEntry? = zipIn.nextEntry
         while (entry != null) {
             var filePath = srcDir.resolve(entry.name)
             if (!entry.isDirectory) {
-                extractFile(zipIn, filePath)
+                filePath.createParentDirectories()
+                Files.newOutputStream(filePath).use { fos ->
+                    readAllBytes(zipIn, fos::write)
+                }
+                filePath.setLastModifiedTime(entry.lastModifiedTime)
             } else {
                 if (!filePath.exists()) {
                     filePath.createDirectories()
@@ -49,6 +65,18 @@ object PackSyncBehavior: SyncBehavior {
             val src = plan.options.src.resolve(file)
             Files.newInputStream(src).use { fis ->
                 val zipEntry = ZipEntry(file.toString())
+                if (plan.options.compress) {
+                    zipEntry.method = ZipEntry.DEFLATED
+                } else {
+                    zipEntry.method = ZipEntry.STORED
+                    zipEntry.size = src.fileSize()
+                    val crc32 = CRC32()
+                    src.inputStream().use {
+                        readAllBytes(it, crc32::update)
+                    }
+                    zipEntry.crc = crc32.value
+                }
+                zipEntry.lastModifiedTime = src.getLastModifiedTime()
                 zipOut.putNextEntry(zipEntry)
                 fis.copyTo(zipOut)
             }
@@ -94,7 +122,6 @@ object PackSyncBehavior: SyncBehavior {
     }
 
     override fun restore(plan: SyncPlan) {
-        val srcDir = plan.options.src
         val fileStream = plan.options.dst.inputStream()
 
         if (plan.options.encrypt) {
@@ -110,24 +137,13 @@ object PackSyncBehavior: SyncBehavior {
 
                 CipherInputStream(fileStream, cipher).use { cipherStream ->
                     ZipInputStream(cipherStream).use { zipIn ->
-                        inZipInput(plan, zipIn, srcDir)
+                        inZipInput(plan, zipIn)
                     }
                 }
             }
         } else {
             ZipInputStream(fileStream).use { zipIn ->
-                inZipInput(plan, zipIn, srcDir)
-            }
-        }
-    }
-
-    private fun extractFile(zipIn: ZipInputStream, filePath: Path) {
-        filePath.createParentDirectories()
-        Files.newOutputStream(filePath).use { fos ->
-            val buffer = ByteArray(4096)
-            var len: Int
-            while (zipIn.read(buffer).also { len = it } > 0) {
-                fos.write(buffer, 0, len)
+                inZipInput(plan, zipIn)
             }
         }
     }
