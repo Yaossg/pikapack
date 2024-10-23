c1434be1eb65ae2681a42c343ea6a30468c33e89
Yaossg <yaossg@outlook.com>
2024-10-23 20:43:14 +0800

    more pack sync tests

--- src/test/kotlin/core/TestPackSyncBehavior.kt
+++ src/test/kotlin/core/TestPackSyncBehavior.kt
@@ -17,14 +17,100 @@ class TestPackSyncBehavior {
         fun tempDir() = Files.createTempDirectory("pikapack_restore")
     }
 
+
     @Test
-    fun testPackRefreshAndRestore() {
+    fun testPack() {
         val tempDir = tempDir()
         val tempZip = tempZip()
         val originSrc = resources.resolve("foo")
 
-        val options1 = Options(src= originSrc, dst= tempZip, pack=true, encrypt=true)
-        val options2 = Options(src= tempDir, dst= tempZip, pack=true, encrypt=true)
+        val options1 = Options(src= originSrc, dst= tempZip, pack=true)
+        val options2 = Options(src= tempDir, dst= tempZip, pack=true)
+        val plan1 = SyncPlan(options1)
+        val plan2 = SyncPlan(options2)
+
+        plan1.refresh()
+        assertTrue(tempZip.exists())
+
+        plan2.restore()
+        assertTrue(originSrc.resolve("a.txt").exists())
+        assertTrue(originSrc.resolve("b.md").exists())
+        assertTrue(originSrc.resolve("bar").exists())
+        assertTrue(originSrc.resolve("bar/c.c").exists())
+        assertTrue(tempDir.resolve("a.txt").exists())
+        assertTrue(tempDir.resolve("b.md").exists())
+        assertTrue(tempDir.resolve("bar").exists())
+        assertTrue(tempDir.resolve("bar/c.c").exists())
+        assertEquals(originSrc.resolve("a.txt").readBytes().toList(), tempDir.resolve("a.txt").readBytes().toList())
+        assertEquals(originSrc.resolve("b.md").readBytes().toList(), tempDir.resolve("b.md").readBytes().toList())
+        assertEquals(originSrc.resolve("bar/c.c").readBytes().toList(), tempDir.resolve("bar/c.c").readBytes().toList())
+    }
+
+    @Test
+    fun testPackCompressed() {
+        val tempDir = tempDir()
+        val tempZip = tempZip()
+        val originSrc = resources.resolve("foo")
+
+        val options1 = Options(src= originSrc, dst= tempZip, pack=true, compress=true)
+        val options2 = Options(src= tempDir, dst= tempZip, pack=true, compress=true)
+        val plan1 = SyncPlan(options1)
+        val plan2 = SyncPlan(options2)
+
+        plan1.refresh()
+        assertTrue(tempZip.exists())
+
+        plan2.restore()
+        assertTrue(originSrc.resolve("a.txt").exists())
+        assertTrue(originSrc.resolve("b.md").exists())
+        assertTrue(originSrc.resolve("bar").exists())
+        assertTrue(originSrc.resolve("bar/c.c").exists())
+        assertTrue(tempDir.resolve("a.txt").exists())
+        assertTrue(tempDir.resolve("b.md").exists())
+        assertTrue(tempDir.resolve("bar").exists())
+        assertTrue(tempDir.resolve("bar/c.c").exists())
+        assertEquals(originSrc.resolve("a.txt").readBytes().toList(), tempDir.resolve("a.txt").readBytes().toList())
+        assertEquals(originSrc.resolve("b.md").readBytes().toList(), tempDir.resolve("b.md").readBytes().toList())
+        assertEquals(originSrc.resolve("bar/c.c").readBytes().toList(), tempDir.resolve("bar/c.c").readBytes().toList())
+    }
+
+    @Test
+    fun testPackEncrypted() {
+        val tempDir = tempDir()
+        val tempZip = tempZip()
+        val originSrc = resources.resolve("foo")
+
+        val options1 = Options(src= originSrc, dst= tempZip, pack=true, encrypt=true, encryptionKey = "123")
+        val options2 = Options(src= tempDir, dst= tempZip, pack=true, encrypt=true, encryptionKey = "123")
+        val plan1 = SyncPlan(options1)
+        val plan2 = SyncPlan(options2)
+
+        plan1.refresh()
+        assertTrue(tempZip.exists())
+
+        plan2.restore()
+        assertTrue(originSrc.resolve("a.txt").exists())
+        assertTrue(originSrc.resolve("b.md").exists())
+        assertTrue(originSrc.resolve("bar").exists())
+        assertTrue(originSrc.resolve("bar/c.c").exists())
+        assertTrue(tempDir.resolve("a.txt").exists())
+        assertTrue(tempDir.resolve("b.md").exists())
+        assertTrue(tempDir.resolve("bar").exists())
+        assertTrue(tempDir.resolve("bar/c.c").exists())
+        assertEquals(originSrc.resolve("a.txt").readBytes().toList(), tempDir.resolve("a.txt").readBytes().toList())
+        assertEquals(originSrc.resolve("b.md").readBytes().toList(), tempDir.resolve("b.md").readBytes().toList())
+        assertEquals(originSrc.resolve("bar/c.c").readBytes().toList(), tempDir.resolve("bar/c.c").readBytes().toList())
+    }
+
+
+    @Test
+    fun testPackCompressedEncrypted() {
+        val tempDir = tempDir()
+        val tempZip = tempZip()
+        val originSrc = resources.resolve("foo")
+
+        val options1 = Options(src= originSrc, dst= tempZip, pack=true, compress = true, encrypt=true, encryptionKey = "123")
+        val options2 = Options(src= tempDir, dst= tempZip, pack=true, compress = true, encrypt=true, encryptionKey = "123")
         val plan1 = SyncPlan(options1)
         val plan2 = SyncPlan(options2)
 
